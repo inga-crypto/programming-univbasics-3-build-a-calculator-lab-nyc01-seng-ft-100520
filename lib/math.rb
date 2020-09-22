b7c8049a1d00aaa11c8d905c39b13a98977bb96a
Inga <ijaanimets@gmail.com>
2020-09-22 03:14:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,15 +9,18 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  div = num2 / num1
+  div
 end
 
 def multiplication(num1, num2)
-
+ multi = num1 * num2
+ multi
 end
 
 def modulo(num1, num2)
-
+ mod = num2 % num1
+ mod
 end
 
 def square_root(num)
